16f2e9f91be812d66ba90ed8b4a662fe3bb4ed48
Daniel <donybof1@gmail.com>
2024-09-20 06:54:01 +0300

    Finished Mission

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Stack<int> Stack = new Stack<int>();
+        while (Stack.Count < 6)
+        {
+            Console.WriteLine("Enter a number");
+            Stack.Push(int.Parse(Console.ReadLine()!));
+        }
+        while (Stack.Count > 0)
+        {
+            Console.WriteLine(Stack.Pop());
+        }
+    }
+    public static Stack<int> ReverseStack(Stack<int> Stack)
+    {
+        Stack<int> Reversed = new Stack<int>();
+        while (Stack.Count > 0)
+        {
+            Reversed.Push(Stack.Pop());
+        }
+        return Reversed;
+    }
+}
